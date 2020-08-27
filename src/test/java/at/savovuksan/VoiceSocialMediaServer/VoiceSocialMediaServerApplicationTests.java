adfa0b5f5d8163e093f823b1670774d5f5957042
Savo Vuksan <savovuksan@gmail.com>
2020-08-27 17:29:08 +0200

    Initial Commit

---
+++ src/test/java/at/savovuksan/VoiceSocialMediaServer/VoiceSocialMediaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.savovuksan.VoiceSocialMediaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceSocialMediaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
