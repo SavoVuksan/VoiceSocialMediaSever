b48aadc4bbff598de851d2d66122f8a263063d4c
Savo Vuksan <savovuksan@gmail.com>
2020-09-25 21:42:17 +0200

    Added Mysql connection

--- src/main/java/at/savovuksan/VSMServer/VSMServer.java
+++ src/main/java/at/savovuksan/VSMServer/VSMServer.java
@@ -3,15 +3,10 @@ package at.savovuksan.VSMServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-
 @SpringBootApplication
 public class VSMServer {
 	public static void main(String[] args) {
 		SpringApplication.run(VSMServer.class, args);
 	}
-
-	
 
 }
