70ec40f53a7ee44d309860a020da804b542afcd6
Savo Vuksan <savovuksan@gmail.com>
2020-09-25 21:34:08 +0200

    Refactored names

---
+++ src/main/java/at/savovuksan/VSMServer/VSMServer.java
@@ -0,0 +1,17 @@
+package at.savovuksan.VSMServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class VSMServer {
+	public static void main(String[] args) {
+		SpringApplication.run(VSMServer.class, args);
+	}
+
+	
+
+}
